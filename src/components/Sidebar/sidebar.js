9a41c1a352221650cf17a4b06a388094e45f4cf9
Ghazal <ghazalnaser2017@gmail.com>
2025-04-25 18:53:58 +0300

    Aspect and subaspects

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Sidebar = () => {
   return (
     <aside className="w-64 bg-white p-4 shadow-md flex flex-col space-y-6 text-gray-700">
@@ -26,8 +27,23 @@ const Sidebar = () => {
             alt="Icon"
             className="absolute left-2 h-4 w-4"
           />
+
           <select className="w-full mt-2 pl-8 p-2 border-b border-gray-400 text-sm bg-transparent focus:outline-none">
-            <option>Category</option>
+            <option value="" disabled selected>
+              Category
+            </option>
+            <option value="option1">Residential Architecure</option>
+            <option value="option2">Interior Design</option>
+            <option value="option3">Cultural Architecture</option>
+            <option value="option4">Commercial & Offices</option>
+            <option value="option5">Public architecture</option>
+            <option value="option6">Commercial & Offices</option>
+            <option value="option7">HealthCare Architecture</option>
+            <option value="option8">Educational Architecture</option>
+            <option value="option9">Sports Architecture</option>
+            <option value="option10">Religious Architecture</option>
+            <option value="option11">Industrial &Infrastructer</option>
+            <option value="option12">Landscape & Urbanism </option>
           </select>
         </div>
         <div className="flex justify-center mt-4">
@@ -49,7 +65,21 @@ const Sidebar = () => {
             className="absolute left-2 h-4 w-4"
           />
           <select className="w-full mt-2 pl-8 p-2 border-b border-gray-400 text-sm bg-transparent focus:outline-none">
-            <option>Category</option>
+            <option value="" disabled selected>
+              Category
+            </option>
+            <option value="option1">Residential Architecure</option>
+            <option value="option2">Interior Design</option>
+            <option value="option3">Cultural Architecture</option>
+            <option value="option4">Commercial & Offices</option>
+            <option value="option5">Public architecture</option>
+            <option value="option6">Commercial & Offices</option>
+            <option value="option7">HealthCare Architecture</option>
+            <option value="option8">Educational Architecture</option>
+            <option value="option9">Sports Architecture</option>
+            <option value="option10">Religious Architecture</option>
+            <option value="option11">Industrial &Infrastructer</option>
+            <option value="option12">Landscape & Urbanism </option>
           </select>
         </div>
         <div className="relative flex items-center">
@@ -59,7 +89,9 @@ const Sidebar = () => {
             className="absolute left-2 h-7 w-4"
           />
           <select className="w-full mt-2 pl-8 p-2 border-b border-gray-400 text-sm bg-transparent focus:outline-none">
-            <option>Sub-aspect</option>
+            <option value="" disabled selected>
+              Sub-aspect
+            </option>
           </select>
         </div>
         <div className="flex justify-center mt-4">
@@ -74,6 +106,42 @@ const Sidebar = () => {
           <img src="/assets/Marker.png" alt="Marker" className="h-6 w-6" />
           <span className="text-sm">Create marker</span>
         </div>
+
+        <div className="relative flex items-center">
+        <img
+            src="/assets/Aspect.png"
+            alt="Icon"
+            className="absolute left-2 h-4 w-4"
+          />
+          <input
+            type="text"
+            placeholder="Aspect"
+            className="w-full mt-2 pl-8 p-2 border-b border-gray-400 text-sm focus:outline-none"
+          />
+        </div>
+        <div className="relative flex items-center">
+          <img
+            src="/assets/Sub-Aspect.png"
+            alt="Icon"
+            className="absolute left-2 h-7 w-4"
+          />
+          <select className="w-full mt-2 pl-8 p-2 border-b border-gray-400 text-sm bg-transparent focus:outline-none">
+            <option value="" disabled selected>
+              Sub-aspect
+            </option>
+          </select>
+        </div>
+        <div className="relative flex items-center">
+          <img
+            src="/assets/Category.png"
+            alt="Icon"
+            className="absolute left-2 h-4 w-4"
+          />
+          <select className="w-full mt-2 pl-8 p-2 border-b border-gray-400 text-sm bg-transparent focus:outline-none">
+            <option>Category</option>
+          </select>
+        </div>
+
         <div className="relative flex items-center">
           <img
             src="/assets/Pen.png"
@@ -86,16 +154,7 @@ const Sidebar = () => {
             className="w-full mt-2 pl-8 p-2 border-b border-gray-400 text-sm focus:outline-none"
           />
         </div>
-        <div className="relative flex items-center">
-          <img
-            src="/assets/Category.png"
-            alt="Icon"
-            className="absolute left-2 h-4 w-4"
-          />
-          <select className="w-full mt-2 pl-8 p-2 border-b border-gray-400 text-sm bg-transparent focus:outline-none">
-            <option>Category</option>
-          </select>
-        </div>
+
         <div className="relative flex items-center">
           <img
             src="/assets/Descrip.png"
@@ -115,9 +174,23 @@ const Sidebar = () => {
             className="absolute left-2 h-4 w-4"
           />
           <input
-            type="file"
-            className="w-full pl-8 p-2 border-b border-gray-400 text-sm bg-transparent focus:outline-none"
+            type="image"
+            className="relative flex items-center mt-2"
+            alt=""
+            id="upload"
+            accept="image/*"
+            placeholder="Upload Image"
           />
+
+
+          <label
+            htmlFor="upload"
+            className="absolute left-10 text-gray-500 text-sm mt-1 cursor-pointer"
+          >
+          
+            Upload Image
+          </label>
+
         </div>
 
         <div className="flex space-x-2 mt-3">
